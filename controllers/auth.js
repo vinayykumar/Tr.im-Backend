69534b5551351e269970eeea48d1afdef6cfa818
Vinay Kumar <Vinayy1709@gmail.com>
2025-08-22 02:23:36 +0530

    updated controllers

---
+++ controllers/auth.js
@@ -0,0 +1,261 @@
+const jwt = require("jsonwebtoken");
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
+const User = require('../models/user')
+const {generateAccessToken, generateRefreshToken} = require('../utils/jwt')
+const crypto = require("crypto");
+const bcrypt = require('bcrypt')
+const sendEmail = require('../utils/sendEmail')
+const {signupSchema, loginSchema} = require('../middlewares/validators')
+
+function generateOTP() {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+async function handleSignupUser(req,res) {
+    const { error } = signupSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    try {
+        const { username, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: "Email already registered" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const otp = generateOTP();
+        const otpExpiry = Date.now() + 10 * 60 * 1000; // 10 min expiry
+
+        //Creating new User in DB
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+            resetPasswordToken : undefined,
+            resetPasswordExpires : undefined,
+            isVerified: false,
+            otp,
+            otpExpiry
+        });
+        
+        await newUser.save();
+
+        // TODO: Send OTP via email
+        await sendEmail(email, "Resend OTP", `Your new OTP is: ${otp}`);
+
+        res.json({ message: "Signup successful. Verify your email with OTP." });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Signup failed" });
+    }
+}
+
+async function handleVerifyOTP(req,res) {
+    try {
+        const { email, otp } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ error: "User not found" });
+        if (user.isVerified) return res.json({ message: "User already verified" });
+
+        if (user.otp !== otp || user.otpExpiry < Date.now()) {
+        return res.status(400).json({ error: "Invalid or expired OTP" });
+        }
+
+        user.isVerified = true;
+        user.otp = undefined;
+        user.otpExpiry = undefined;
+
+        // Generate tokens
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+        user.refreshTokens.push(refreshToken);
+
+        await user.save();
+
+        res.json({ message: "Email verified successfully", accessToken, refreshToken });
+  } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "OTP verification failed" });
+  }
+}
+
+async function handleResendOTP(req,res) {
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ error: "User not found" });
+        if (user.isVerified) return res.json({ message: "User already verified" });
+
+        const otp = generateOTP();
+        user.otp = otp;
+        user.otpExpiry = Date.now() + 10 * 60 * 1000;
+
+        await user.save();
+
+        // TODO: Send OTP via email
+        await sendEmail(email, "Resent OTP", `Your new OTP is: ${otp}`);
+        console.log(`Resent OTP for ${email}: ${otp}`);
+
+        res.json({ message: "OTP resent successfully" });
+  } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to resend OTP" });
+  }
+}
+
+async function handleLoginUser(req,res) {
+    const { error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    try {
+        const { email, password } = req.body;
+
+        //Validate User
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ error: "User not found" });
+        if (!user.isVerified) return res.status(403).json({ error: "Email not verified" });
+
+        //Validate Password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(401).json({ error: "Invalid password" });
+
+        //Creating new Tokens
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        user.refreshTokens.push(refreshToken);
+        await user.save();
+
+        return res.json({ accessToken, refreshToken });
+        
+    } catch (err) {
+        res.status(500).json({ error: "Login failed" });
+    }
+}
+
+async function handleForgotPassword(req,res) {
+    try{
+        const {email} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user) return res.status(404).json({error:"User not found"});
+
+        //Generate reset token
+        const resetToken = crypto.randomBytes(32).toString("hex");
+
+        // Save hashed token + expiry in DB
+        user.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+        user.resetPasswordExpires = Date.now() + 1000 * 60 * 15; // 15 minutes
+        await user.save();
+
+        // Send email with link 
+        const resetUrl = `http://my-frontend.com/reset-password/${resetToken}`;
+        await sendEmail(user.email, "Password Reset", `Click here: ${resetUrl}`);
+
+        res.json({ message: "Password reset link sent to email" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+}
+
+async function handleResetPassword(req,res) {
+    try {
+        const { password } = req.body;
+        const { token } = req.params;
+    
+        // Hash the token and find user
+        const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    
+        const user = await User.findOne({
+          resetPasswordToken: hashedToken,
+          resetPasswordExpires: { $gt: Date.now() },
+        });
+    
+        if (!user) return res.status(400).json({ error: "Invalid or expired token" });
+    
+        // Hash new password and save
+        user.password = await bcrypt.hash(password, 10);
+    
+        // Clear reset fields
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+    
+        await user.save();
+    
+        res.json({ message: "Password has been reset successfully" });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong" });
+      }
+}
+
+async function handleRefreshToken(req,res) {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) return res.status(401).json({ error: "Refresh token required" });
+
+    try {
+        // Verify the refresh token
+        const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+
+        // Check if it’s still in DB
+        const user = await User.findById(payload.id);
+        if (!user || !user.refreshTokens.includes(refreshToken)) {
+            return res.status(403).json({ error: "Invalid refresh token" });
+        }
+
+        // Rotate token: remove old one
+        user.refreshTokens = user.refreshTokens.filter((t) => t !== refreshToken);
+
+        // Generate new access token
+        const newAccessToken = generateAccessToken(user);
+        const newRefreshToken = generateRefreshToken(user);
+
+        // Save new refreshToken in DB
+        user.refreshTokens.push(newRefreshToken);
+        await user.save();
+
+        res.json({ accessToken: newAccessToken, refreshToken:newRefreshToken });
+    } catch (err) {
+        console.error(err);
+        res.status(403).json({ error: "Invalid or expired refresh token" });
+    }
+}
+
+async function handleLogoutUser(req,res) {
+    const {refreshToken} = req.body;
+    if(!refreshToken) return res.status(401).json({error:"Refresh token required"});
+
+    try{
+        const payload = jwt.verify(refreshToken,REFRESH_TOKEN_SECRET);
+
+        const user = await User.findById(payload.id);
+        if (!user) return res.status(403).json({ error: "Invalid user" });
+
+        user.refreshTokens = user.refreshTokens.filter((token) => token !== refreshToken);
+        await user.save();
+
+        res.json({ message: "Logged out successfully" });
+    }
+    catch(err){
+        console.error(err);
+        res.status(403).json({ error: "Invalid or expired refresh token" });
+    }
+}
+
+module.exports = {
+    handleSignupUser,
+    handleVerifyOTP,
+    handleResendOTP,
+    handleLoginUser,
+    handleForgotPassword,
+    handleResetPassword,
+    handleRefreshToken,
+    handleLogoutUser
+}
--- routes/url.js
+++ routes/url.js
@@ -3,80 +3,15 @@ const router = express.Router();
 const authenticateToken = require("../middlewares/auth");
 const URL = require("../models/url");
 const {shortenLimiter} = require('../middlewares/ratelimiter')
-
+const {handleMyUrls, handleGetOriginalUrl, handleGetShortUrl} = require('../controllers/url')
 
 // Protected route: create a short URL
-router.post("/shorten", shortenLimiter, authenticateToken, async (req, res) => {
-    try {
-        const { redirectedURL, ttl } = req.body; // optional TTL in hours
-
-        // Generate a simple random short ID
-        const shortID = Math.random().toString(36).substring(2, 8);
-        
-        // Set expiry dynamically: either provided TTL or default 24 hours
-        const expiresAt = ttl 
-            ? new Date(Date.now() + ttl * 60 * 60 * 1000)
-            : new Date(Date.now() + 24 * 60 * 60 * 1000);
-
-        const newURL = await URL.create({
-            shortID,
-            redirectedURL,
-            userID: req.user.id,
-            visitHistory: [],
-            expiresAt
-        });
-
-        res.json(newURL);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Could not create short URL" });
-    }
-});
+router.post("/shorten", shortenLimiter, authenticateToken, handleGetShortUrl);
 
 //Public route : get Redirected/Original URL
-router.get('/:shortID', async (req,res)=> {
-    try{
-        const {shortID} = req.params;
-
-        const urlRecord =  await URL.findOne({shortID});
-        if(!urlRecord) return res.status(404).json({error : "Short URL not found"})
-
-        urlRecord.visitHistory.push({
-            timestamp : Date.now(),
-            ip : req.ip,
-            userAgent : req.headers["user-agent"],
-            referrer : req.get("Referrer") || "Direct"
-        });
-
-        await urlRecord.save();
-
-        res.redirect(urlRecord.redirectedURL);
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).json({error : "Could not redirect"})
-    }
-});
+router.get('/:shortID', handleGetOriginalUrl);
 
 //Protected route : get all urls of current user with userID
-router.get('/my-urls', authenticateToken, async (req,res) => {
-    try{
-        const urls = await URL.find({userID : req.params.id}).lean();
-
-        const data = urls.map((url) => ({
-            shortID: url.shortID,
-            redirectedURL: url.redirectedURL,
-            createdAt: url.createdAt,
-            expiresAt: url.expiresAt,
-            totalClicks: url.visitHistory.length,
-            visits: url.visitHistory   // full visit details
-        }));
-
-        return res.json(data)
-    }
-    catch(err){
-        return res.status(500).json({ error: "Could not fetch URLs" });
-    }
-})
+router.get('/my-urls', authenticateToken,handleMyUrls)
 
 module.exports = router;
